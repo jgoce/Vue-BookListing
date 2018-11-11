5748eb6894a4d4020cd4b9711ffdafc783589ab2
Goce Jankovski <j.goce@yahoo.com>
2018-11-11 10:02:12 -0500

    Update src/components/BookForm.vue

---
+++ src/components/BookForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
+        <input v-model="bookTitle" type="text" name="title" value="" placeHolder="Book Title" />
+        <input v-model="bookAuthor" type="text" name="author" value="" placeHolder="Book Author" />
+        <button type="submit" name="button">Add Book</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'BookForm',
+    props: ['books'],
+    data() {
+        return {
+            bookTitle: '',
+            bookAuthor: ''
+        }
+    },
+    methods: {
+        bookSubmit(bookTitle, bookAuthor){
+            this.$emit('addBook', bookTitle, bookAuthor)
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
